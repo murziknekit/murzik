b13ebfb688b88647363d9695e681d3152f88bf9b
murziknekit <152699704+murziknekit@users.noreply.github.com>
2023-12-16 14:12:09 +0300

    Пример

---
+++ lesson3.py
@@ -0,0 +1,17 @@
+word = 'hel lo nisdjgo jgsopgjsdopgs gjseopgjsdpog soj sopgs'
+word2 = 'world '
+print((word + word2)*3)
+print(word + '4')
+
+print(len(word))
+
+array = word.split('.')
+print(word.split('g'))
+
+print(array[0])
+
+if 'gsojfgjfg' in word:
+    print('we find "gso')
+else:
+    print('we dont find')
+    
