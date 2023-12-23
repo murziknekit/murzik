4ec7e075e835b6ced3365f5f806d5f1c47ebba6e
murziknekit <152699704+murziknekit@users.noreply.github.com>
2023-12-23 12:12:46 +0300

    qq

---
+++ dz.py.py
@@ -0,0 +1,9 @@
+'''
+
+                            Online Python Debugger.
+                Code, Run and Debug Python program online.
+Write your code in this editor and press "Debug" button to debug program.
+
+'''
+
+print("Hello World")
